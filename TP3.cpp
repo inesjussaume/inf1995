597cbe2c89bf78f5f433ba7aa42561c9db46b6c8
mamarbb <mamarbb@l3818-18.info.polymtl.ca>
2018-01-22 15:45:52 -0500

    commit du tp.cpp

---
+++ TP3.cpp
@@ -0,0 +1,57 @@
+/*
+TP3 Mathieu Marchand et Ines Jussaume
+*/
+
+/*Table a etat :
+ * 
+ * 
+ * */
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+//#define F_CPU = 16000000
+
+public enum Etat{
+    ETATETEINT,
+    ETAT1,
+    ETAT2,
+    ETAT3,
+    ETAT4,
+    ETAT5,
+    ETATALLUMER
+}
+
+typedef  
+
+bool antiRebond();
+int main (){
+  DDRA = 0xff; // PORT A est en mode sortie
+  DDRB = 0xff; // PORT B est en mode sortie
+  DDRC = 0xff; // PORT C est en mode sortie
+  DDRD = 0x00; // PORT D est en mode entree
+  bool allumer = false;
+  while(true){
+    if(!allumer){
+      allumer = antiRebond();
+    }else {//si allumer
+      PINA = 1;//0000001 
+      //0000010
+      _delay_ms(100);
+      PINA = 2;
+  }
+      
+}
+return 0;
+}
+
+bool antiRebond(){
+      if(PIND & (1 << 2)){//si le bouton est active
+      _delay_ms(10);
+      if(PIND & (1 << 2)){//si le bouton est toujours /////active apres 1 sec
+         return true;
+      }
+    }
+    return false;
+}
