1b09da8983d214fa95a5466014b09c2dc96f3353
mamarbb <mamarbb@polymtl.ca>
2018-01-25 14:26:27 -0500

    reste a corriger le bug de la lumiere rouge qui n'allume pas pour le probleme 2

--- TP3-2/TP3-2.cpp
+++ TP3-2/TP3-2.cpp
@@ -4,11 +4,98 @@ TP3 probleme 2
 
 #include <avr/io.h>
 #include <util/delay.h>
+///
+
+const uint8_t ETEINT = 0b00000000;
+const uint8_t VERT = 0b00000001;
+const uint8_t ROUGE = 0b00000010;
+
+enum Etat{
+	RELACHE_DEPART_ROUGE,
+	PESE_AMBRE,
+	RELACHE_VERT,
+	PESE_ROUGE,
+	RELACHE_ETEINT,
+	PESE_VERT
+};
+
+bool antiRebond();
+void etatSuivant(Etat &etatActuel);
+//void switchEtat(Etat &etatActuel);
 
 int main(){
-
-while(true){
+	DDRA = 0xff; //PORT A est en sortie
+	DDRB = 0xff; //PORT B en sortie
+	DDRC = 0xff; //PORT C en sortie
+	DDRD = 0x00; //PORT D en entree
+	bool enTrainPeser = false;
+	Etat etat = RELACHE_DEPART_ROUGE;
+	PINA = ROUGE ;
 	
+while(true){
+
+	if(etat == PESE_AMBRE){
+	PINA = ETEINT;
+	PINA = ROUGE;
+	_delay_ms(100);
+	PINA = VERT;
+	_delay_ms(100);
+	PINA = PINA;
+
+	_delay_ms(100);
+	}
+		
+	if(antiRebond() && !enTrainPeser){
+	enTrainPeser = true;
+	etatSuivant(etat);
+	}
+
+	if(!antiRebond() && enTrainPeser){
+	enTrainPeser = false;
+	etatSuivant(etat);	
+	}
+		
 }
+
 return 0;
+}
+
+void etatSuivant(Etat &etatActuel){
+	switch(etatActuel){
+		case RELACHE_DEPART_ROUGE:
+			etatActuel = PESE_AMBRE;
+			break;
+		case PESE_AMBRE:
+			etatActuel = RELACHE_VERT;
+			PINA = VERT;
+			break;
+		case RELACHE_VERT:
+			etatActuel = PESE_ROUGE;
+			PINA = ROUGE;
+			break;
+		case PESE_ROUGE:
+			etatActuel = RELACHE_ETEINT;
+		       	PINA = PINA;//pour eteindre (point d'interrogation
+			break;
+		case RELACHE_ETEINT:
+		       etatActuel = PESE_VERT;
+		       PINA = VERT;
+		       break;
+		case PESE_VERT:
+			etatActuel = RELACHE_DEPART_ROUGE;
+			PINA=ROUGE;
+	
+	}
+
+}
+
+
+bool antiRebond(){
+      if(PIND & (1 << 2)){//si le bouton est active
+      _delay_ms(10);
+      if(PIND & (1 << 2)){//si le bouton est toujours /////active apres 1 sec
+         return true;
+      }
+    }
+    return false;
 }
