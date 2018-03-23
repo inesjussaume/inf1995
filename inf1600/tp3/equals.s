a37e01097f8a05e5b21f9aa70aaa93e6bf614967
Mathieu Marchand <mamarbb@polymtl.ca>
2018-03-23 00:49:13 -0400

    equals fonctionne et transpose a un lege bug en ce qui concerne les range du bas

--- inf1600/tp3/equals.s
+++ inf1600/tp3/equals.s
@@ -1,46 +1,50 @@
 .globl matrix_equals_asm
 
 matrix_equals_asm:
-        push %ebp      /* Save old base pointer */
+       push %ebp      /* Save old base pointer */
         mov %esp, %ebp /* Set ebp to current esp */
-        
-        /* Write your solution here */
-        mov $0,%esi        #r dans ESI
-       
-        mov 8(%ebp),%ecx   #inmatdata1 dans ecx
-        mov 12(%ebp),%ebx  #inmatdata2 dans ebx
-        
-        
-boucle1:
-    mov $0,%edi            #c dans EDI             
-    cmp %esi,16(%ebp)      
-    je fin1                #r - mat <= 0 alors jump fin   
-    inc %esi  #++r
 
+        
+        
+        mov $0,%esi        #r dans ESI
+        mov $-1,%edi 
+       
+boucle1:
+    inc %esi  #++r         #c dans EDI             
+    cmp $2,%esi      
+    je fin1                #r - mat <= 0 alors jump fin   
+    
+ 
 boucle2:
    
-    cmp %edi,16(%ebp)           #c - mat  <= 0 alors jump boucle1
+    cmp $2,%edi         #c - mat  <= 0 alors jump boucle1
     je boucle1
-    inc %edi
 
- if:   
+equals:
     mov 16(%ebp),%edx       #matorder dans %edx
+    mov 8(%ebp),%ecx        #ecx = inmatdata1
+    mov 12(%ebp),%ebx       #ebx = inmatdata2
     movl %esi,%eax          #eax = r
     mull %edx               #eax = matorder * r 
     add %edi, %eax          #eax = matorder * r +c
-    pushl (%ecx,%eax,4)     #-4(%ebp) = inmatdata1[c + r * matorder]
+    
+    pushl (%ecx,%eax,4)   #-4(%ebp) = inmatdata1[c + r * matorder]
     pushl (%ebx,%eax,4)     #-8(ebp) = inmatdata2[c + r * matorder]
     pop %eax
     pop %edx
+    
+    inc %edi
     cmp %eax,%edx
     je boucle2              #si egale, recommence la boucle2
-    
     movl $0,%eax
+ 
 fin: 
+    #mov %esi,%eax
+    #mull %edi
+    #add $12,%eax
     leave          /* Restore ebp and esp */
     ret            /* Return to the caller */
 fin1 :
     movl $1,%eax
     jmp fin
-    
-    
+     
--- inf1600/tp3/transpose.s
+++ inf1600/tp3/transpose.s
@@ -5,6 +5,49 @@ matrix_transpose_asm:
         mov %esp, %ebp /* Set ebp to current esp */
         
         /* Write your solution here */
+        movl $-1,%esi   #esi = r
+        movl $0,%edi    #edi = c 
+        
+boucle1:
+    inc %esi  #++r         #r dans ESI             
+    cmp 16(%ebp),%esi      
+    je fin                #r - mat <= 0 alors jump fin   
+    
+ 
+boucle2:
+   
+    cmp 16(%ebp),%edi         #c - mat  <= 0 alors jump boucle1
+    je boucle1
+
+equals:
+    mov 16(%ebp),%edx       #matorder dans %edx
+    mov 8(%ebp),%ecx        #ecx = inmatdata
+    mov 12(%ebp),%ebx       #ebx = outmatdata
+    movl %edi,%eax          #eax = c
+    mull %edx               #eax = matorder * c 
+    add %esi, %eax          #eax = matorder * c + r
+    
+    pushl (%ecx,%eax,4)   #-4(%ebp) = inmatdata1[r + c * matorder]
+    
+    
+    mov 16(%ebp),%edx       #matorder dans %edx
+    movl %esi,%eax          #eax = r
+    mull %edx               #eax = matorder * r 
+    addl %edi, %eax         #eax = matorder * r + c   
+    
+    
+    pop %ecx                #ecx = inmatdata1[r + c * matorder]
+    
+   
+    movl %ecx,(%ebx,%eax,4)
+    
+
+    
+    inc %edi
+
+    jmp boucle2              #si egale, recommence la boucle2
+    
+fin:
 
         leave          /* Restore ebp and esp */
         ret            /* Return to the caller */
