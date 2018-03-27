e3228e1bc8a1bdae3205d41db6575e79e6f7c7c8
Ines Jussaume <ines.jussaume@hotmail.com>
2018-03-27 11:32:46 -0400

    triangle height

--- inf1600/tp4/triangle_height.s
+++ inf1600/tp4/triangle_height.s
@@ -13,20 +13,29 @@
 _ZNK9CTriangle9HeightAsmEv:
         push %ebp               /* save old base pointer */
         mov %esp, %ebp          /* set ebp to current esp */
-        /*
+        
         mov 8(%ebp), %eax       # objet triangle dans %eax
         mov 0(%eax), %ecx       # VTable dans %ecx pour pouvoir faire l'appel de fonction
         
-        #push %eax
+        push %eax               # enregistrer l'objet triangle
+                                # 0(%eax)  --> VTable 
+                                # 4(%eax)  --> mSides[0]
+                                # 8(%eax)  --> mSides[1]
+                                # 12(%eax) --> mSides[2]
         
-        call *16(%ebx)          # appel de la fonction ArecCpp()
-        fld factor              # factor sur la pile st[0]=factor
-        fmulp                   # multiplication st[0] = st[1] * st[0]
-                                #                st[0] = 2.0f  *  A
+        call *16(%ecx)          # appel de la fonction AreaCpp()
+                                 
+        mov 4(%eax), %ecx       # mSides[ ] dans %ecx
+                                
         fld 12(%eax)            # mSides[2] sur la pile
         fdivrp                  # division st[0] = st[1]  / st[0]
-                                #          st[0] = 2.0f*a / mSides[2]
-        
-      */
+                                #          st[0] =   A    / mSides[2] 
+                                
+        fld factor              # factor sur la pile st[0]=factor
+        fmulp                   # multiplication st[0] = st[1] * st[0]
+                                #                st[0] = (A / mSides[2]) * 2.0f
+      
+                                
+        pop %eax
         leave                   /* restore ebp and esp */
         ret                     /* return to the caller */
