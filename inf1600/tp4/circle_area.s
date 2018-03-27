bd4290b857856651467403b330eb977b3320796c
Ines Jussaume <ines.jussaume@hotmail.com>
2018-03-27 10:48:53 -0400

    circle area et tirangle height

--- inf1600/tp4/circle_area.s
+++ inf1600/tp4/circle_area.s
@@ -1,10 +1,17 @@
 .globl _ZNK7CCircle7AreaAsmEv
 
 _ZNK7CCircle7AreaAsmEv:
-        push %ebp      /* save old base pointer */
-        mov %esp, %ebp /* set ebp to current esp */
+        push %ebp               /* save old base pointer */
+        mov %esp, %ebp          /* set ebp to current esp */
         
-        /* Write your solution here */
+        sub $4, %esp            # ajoute espace pour utiliser rayon
         
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+        fldpi                   # pi est sur la pile
+        mov 8(%ebp), %eax       # cercle mis dans %eax
+        fld 4(%eax)             # rayon sur la pile
+        fmulp                   # multiplication pi * rayon
+        fld 4(%eax)             # remettre rayon sur pile
+        fmulp                   # pi*rayon*rayon
+    
+        leave                   /* restore ebp and esp */
+        ret                     /* return to the caller */
--- inf1600/tp4/triangle_height.s
+++ inf1600/tp4/triangle_height.s
@@ -1,10 +1,32 @@
+# float CTriangle::HeightCpp() const {
+#	float A = AreaCpp();
+#	return 2.0f*A/mSides[2];
+#}
+
+.data
+        factor: .float 2.0 
+.text
+
 .globl	_ZNK9CTriangle9HeightAsmEv
 
+
 _ZNK9CTriangle9HeightAsmEv:
-        push %ebp      /* save old base pointer */
-        mov %esp, %ebp /* set ebp to current esp */
+        push %ebp               /* save old base pointer */
+        mov %esp, %ebp          /* set ebp to current esp */
         
-        /* Write your solution here */
+        mov 8(%ebp), %eax       # objet triangle dans %eax
+        mov 0(%eax), %ecx       # VTable dans %ecx pour pouvoir faire l'appel de fonction
         
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+        #push %eax
+        
+        call *16(%ebx)          # appel de la fonction ArecCpp()
+        fld factor              # factor sur la pile st[0]=factor
+        fmulp                   # multiplication st[0] = st[1] * st[0]
+                                #                st[0] = 2.0f  *  A
+        fld 12(%eax)            # mSides[2] sur la pile
+        fdivrp                  # division st[0] = st[1]  / st[0]
+                                #          st[0] = 2.0f*a / mSides[2]
+        
+        
+        leave                   /* restore ebp and esp */
+        ret                     /* return to the caller */
