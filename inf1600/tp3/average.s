9234626fe2a6189f5d886657fb168570bf1ccbec
Ines Jussaume <ines.jussaume@hotmail.com>
2018-03-26 21:13:33 -0400

    test

--- inf1600/tp3/average.s
+++ inf1600/tp3/average.s
@@ -8,8 +8,8 @@ matrix_row_aver_asm:
     subl $8, %esp                       # allocation de l'espace mémoire pour les variables locale
     pusha
   	
-    movl $0, %esi                       # r = 0 dans esi
-    movl $0, %edi                       # c = 0 dans edi
+    movl $0, %esi                       # r = 0 dans %esi
+    movl $0, %edi                       # c = 0 dans %edi
 
 
 
@@ -17,7 +17,7 @@ for1:                                   # r plus petit que matorder
 # for(r = 0; r < matorder; ++r)
 # 	elem = 0;
 
-    cmp %esi 16(%ebp)                  # comparaison de matorder et r
+    cmp %esi, 16(%ebp)                  # comparaison de matorder et r
     movl $0, -4(%ebp)					# elem = 0
 	jle end                           	# jump to end if r = matorder
     movl $0, %edi                       # c remis à 0
@@ -28,15 +28,15 @@ for2:                                   # c plus petit que matorder
 # 	elem += inmatdata[c + r * matorder]
 
     cmp %edi, 16(%ebp)                  # comparaison de c et de matorder 
-    jle outmatdataR                      # jump to outmatdataR if c = matorder
+    jle outmatdataR                     # jump to outmatdataR if c = matorder
 
     # elem += inmatdata[c + r * matorder]
-    movl 8(%ebp), %ecx					# inmatdata dans %ecx
-    movl 12(%ebp), %ebx					# outmatdata dans %ebx
+
     movl %esi, %eax                     # r est mis dans %eax
     mull 16(%ebp)                       # multiplication de r et de matorder
     addl %edi, %eax                     # c + r * matorder
-    movl (%ecx,%eax,4), %eax            # inmatdata[ c + r * matorder ]
+    movl 8(%ebp), %ebx					# inmatdata dans %ebx                            ####
+    movl (%ebx,%eax,4), %eax            # inmatdata[ c + r * matorder ]
     addl %eax, -4(%ebp)                 # elem += inmatdata[ c + r * matorder ]
 
     incl %edi                           # c est incrémenté
@@ -45,19 +45,20 @@ for2:                                   # c plus petit que matorder
 outmatdataR:
 
     # outmatdata[r] = elem/matorder
-    movl -4(%ebp), %eax                 # elem dans le registre %eax
+    movl -4(%ebp), %eax                 # elem dans le registre %eax (utilise pour la multiplication)
     divl 16(%ebp)                       # division (%eax = elem/matorder)
-    movl %eax, (%ebx,%esi,4)            # outmatdata[r] = elem/matorder;
+    movl 12(%ebp), %ecx					# outmatdata dans %ecx                             #####
+    movl %eax, (%ecx,%esi,4)            # outmatdata[r] = elem/matorder;
 
     movl $0, %edi                       # c remis à 0
     incl %esi                           # r est incrémenté
-    jmp for1                            # jmp au for1
+    jmp for1                            # jmp au debut du for1
 
 end:
     popa
     addl $8, %esp                      # libérer espace variables locales
 
                         
-    leave          			/* Restore ebp and esp */
-    ret           			/* Return to the caller */
+    leave          			           /* Restore ebp and esp */
+    ret           			           /* Return to the caller */
 		
