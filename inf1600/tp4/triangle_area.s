066ac3535d745350f7b0dede6d7b8029eb309cd4
Ines Jussaume <ines.jussaume@polymtl.ca>
2018-03-27 10:44:48 -0400

    commti

--- inf1600/tp4/triangle_area.s
+++ inf1600/tp4/triangle_area.s
@@ -5,6 +5,20 @@ _ZNK9CTriangle7AreaAsmEv:
         mov %esp, %ebp /* set ebp to current esp */
         
         /* Write your solution here */
+        mov 8(%ebp), %eax	#Triangle dans eax
+        mov (%eax), %ebx	#VTable de Triangle dans ebx
+        call *8(%ebx)		#Appele la fonction PerimeterCpp()
         
-        leave          /* restore ebp and esp */
-        ret            /* return to the caller */
+        fld 2			#st[0] = 2.0f et st[1] = PerimeterCpp() 
+        
+        fdivrp 				#st[0] = st[1] = PerimeterCpp() /2.0f
+        
+        #fstp %ecx			#ecx = p
+        
+        
+        
+        
+        
+        
+        leave         		/* restore ebp and esp */
+        ret            		/* return to the caller */
--- inf1600/tp4/triangle_perimeter.s
+++ inf1600/tp4/triangle_perimeter.s
@@ -6,14 +6,14 @@ _ZNK9CTriangle12PerimeterAsmEv:
         
         /* Write your solution here */
         mov 8(%ebp),%eax		#objet dans eax
-        mov 4(%eax),%ebx 		#mSides[3] dans eax
-        mov $0,%eax
-        fld (%ebx,%eax,4)		#met sur la pile mSide[0]
-        inc %eax				#%eax++
-        fld (%ebx,%eax,4)		#met sur la pile mSide[1]
+        mov 4(%eax),%ebx 		#mSides[3] dans ebx
+        mov $1,%ebx				#1 dans #ebx
+        fld (%eax,%ebx,4)		#met sur la pile mSide[0]
+        inc %ebx				#%ebx++
+        fld (%eax,%ebx,4)		#met sur la pile mSide[1]
         faddp 					#mSides[0] + mSides[1] = st[0] = st[1]
-        inc %eax				#%eax++
-        fld (%ebx,%eax,4)		#met sur la pile mSide[2]
+        inc %ebx				#%ebx++
+        fld (%eax,%ebx,4)		#met sur la pile mSide[2]
         faddp
         leave          			/* restore ebp and esp */
         ret            			/* return to the caller */
