0f5abd7591bdd35b512cc5addcee794a63b19662
mamarbb <mamarbb@polymtl.ca>
2018-04-10 10:37:23 -0400

    exercice 1 tp5 inf1600 complet

--- inf1600/tp5/TP5-20180410/tp5.c
+++ inf1600/tp5/TP5-20180410/tp5.c
@@ -8,18 +8,21 @@ unsigned int Decryption_fct(unsigned int le)
 	/*
 	 * Remplacez le code suivant par de l'assembleur en ligne
 	 * en utilisant le moins d'instructions possible
-	 
 	 be = (le & 0xff000000) | (le&0xff) << 16  | (le & 0xff00) | (le & 0xff0000) >> 16;
-	 */
-	
+	 	 */
+
+
 
 	asm volatile (
-		// instructions...
-		: // sorties (s'il y a lieu)
-		: // entrées
-		: // registres modifiés (s'il y a lieu)
+	
+		"movl %1, %%eax;"
+		"bswap %%eax;"
+		"ror $8, %%eax;"
+		
+		:"=r"(be)// sorties 
+		:"r"(le)// entrées
+		:"%eax" // registres modifiés 
 	);
-
 	return be;
 }
 
